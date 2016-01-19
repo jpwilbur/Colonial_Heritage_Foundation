4b47d7b231e0c64682372199f2a060e8b725430a
jpwilbur <jpwilbur@hotmail.com>
2016-01-19 15:14:59 -0700

    First portion created for Sprint 1. Skeleton created with blocks for further content to be put in.

--- homepage/cached_templates/templates/base.htm.py
+++ homepage/cached_templates/templates/base.htm.py
@@ -5,13 +5,13 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1452883339.9235137
+_modified_time = 1453241430.1824594
 _enable_loop = True
 _template_filename = 'C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/base.htm'
 _template_uri = 'base.htm'
 _source_encoding = 'utf-8'
 import os, os.path, re, json
-_exports = ['content']
+_exports = ['above_columns', 'center_content', 'right_content', 'title', 'left_content', 'maintenance_message']
 
 
 from django_mako_plus.controller import static_files 
@@ -20,10 +20,21 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        request = context.get('request', UNDEFINED)
+        def above_columns():
+            return render_above_columns(context._locals(__M_locals))
+        def center_content():
+            return render_center_content(context._locals(__M_locals))
+        def title():
+            return render_title(context._locals(__M_locals))
         self = context.get('self', UNDEFINED)
-        def content():
-            return render_content(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def left_content():
+            return render_left_content(context._locals(__M_locals))
+        def right_content():
+            return render_right_content(context._locals(__M_locals))
+        request = context.get('request', UNDEFINED)
+        def maintenance_message():
+            return render_maintenance_message(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\r\n')
         __M_writer('\r\n')
@@ -31,31 +42,128 @@ def render_body(context,**pageargs):
         
         __M_locals_builtin_stored = __M_locals_builtin()
         __M_locals.update(__M_dict_builtin([(__M_key, __M_locals_builtin_stored[__M_key]) for __M_key in ['static_renderer'] if __M_key in __M_locals_builtin_stored]))
-        __M_writer('\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n  <meta charset="UTF-8">\r\n  <head>\r\n    \r\n    <title>homepage</title>\r\n    \r\n')
-        __M_writer('    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\r\n  \r\n')
-        __M_writer('    ')
-        __M_writer(str( static_renderer.get_template_css(request, context)  ))
-        __M_writer('\r\n  \r\n  </head>\r\n  <body>\r\n  \r\n    <header>\r\n      Welcome to the homepage app!\r\n    </header>\r\n  \r\n    ')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
-            context['self'].content(**pageargs)
+        __M_writer('\r\n')
+        from datetime import datetime 
+        
+        __M_locals_builtin_stored = __M_locals_builtin()
+        __M_locals.update(__M_dict_builtin([(__M_key, __M_locals_builtin_stored[__M_key]) for __M_key in ['datetime'] if __M_key in __M_locals_builtin_stored]))
+        __M_writer('\r\n\r\n<!-- every hour i work is netflix for two months. why work? -->\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n  <meta charset="UTF-8">\r\n  <head>\r\n\r\n    <title>')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
+            context['self'].title(**pageargs)
         
 
-        __M_writer('  \r\n  \r\n')
+        __M_writer('</title>\r\n\r\n    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\r\n    <link rel="stylesheet" href="')
+        __M_writer(str(STATIC_URL))
+        __M_writer('homepage\\media\\bootstrap\\css\\bootstrap-theme.min.css">\r\n    <link rel="stylesheet" href="')
+        __M_writer(str(STATIC_URL))
+        __M_writer('homepage\\media\\bootstrap\\css\\bootstrap.min.css">\r\n    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">\r\n    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.7/font-awesome-animation.css">\r\n    <link rel="icon" href="')
+        __M_writer(str(STATIC_URL))
+        __M_writer('homepage/media/CHF.ico"\r\n\r\n')
+        __M_writer('    ')
+        __M_writer(str( static_renderer.get_template_css(request, context)  ))
+        __M_writer('\r\n\r\n  </head>\r\n\r\n\r\n  <div class="alert alert-danger alert-dismissible" role="alert">\r\n    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\r\n    <div class="text-center">\r\n      ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'maintenance_message'):
+            context['self'].maintenance_message(**pageargs)
+        
+
+        __M_writer('\r\n    </div>\r\n  </div>\r\n  <body>\r\n  <header>\r\n    <div class="container-fluid">\r\n      <nav class="navbar navbar-inverse" role="navigation">\r\n        <div class="container-fluid">\r\n          <div class="navbar-header">\r\n            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">\r\n              <span class="sr-only">Toggle navigation</span>\r\n              <span class="icon-bar"></span>\r\n              <span class="icon-bar"></span>\r\n              <span class="icon-bar"></span>\r\n            </button>\r\n            <a class="navbar-brand" href="#">Colonial Heritage Foundation</a>\r\n          </div>\r\n          <!-- Collect the nav links, forms, and other content for toggling -->\r\n          <div class="collapse navbar-collapse" id="navbar">\r\n            <ul class="nav navbar-nav">\r\n              <li><a href="#">Home</a></li>\r\n              <li><a href="#">About</a></li>\r\n              <li><a href="#">Contact</a></li>\r\n              <li class="dropdown">\r\n                <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <b class="caret"></b></a>\r\n                <ul class="dropdown-menu">\r\n                  <li><a href="#">Login</a></li>\r\n                </ul>\r\n              </li>\r\n            </ul>\r\n          </div><!-- /.navbar-collapse -->\r\n        </div>\r\n      </nav>\r\n    </div><!-- /.container-fluid -->\r\n  </header>\r\n  <div class="row">\r\n    <div class="col-md-2" style="float:right">\r\n      <div class="alert alert-danger alert-dismissible" role="alert">\r\n        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\r\n        <strong>Warning!</strong> Better check yourself, you\'re not looking too good.\r\n      </div>\r\n    </div>\r\n  </div>\r\n  <div class="container">\r\n    <div class="row">\r\n      <div class="col-md-12 bg-info text-center">\r\n        <h1>\r\n          ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'above_columns'):
+            context['self'].above_columns(**pageargs)
+        
+
+        __M_writer('\r\n        </h1>\r\n      </div>\r\n    </div>\r\n    <div class="row">\r\n      <div class="text-left col-md-3 bg-danger">\r\n        ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'left_content'):
+            context['self'].left_content(**pageargs)
+        
+
+        __M_writer('\r\n      </div>\r\n\r\n      <div class="text-center col-md-6 bg-warning">\r\n        ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'center_content'):
+            context['self'].center_content(**pageargs)
+        
+
+        __M_writer('\r\n      </div>\r\n\r\n      <div class="text-right col-md-3 bg-primary  ">\r\n        ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'right_content'):
+            context['self'].right_content(**pageargs)
+        
+
+        __M_writer('\r\n      </div>\r\n    </div>\r\n  </div>\r\n  <div class="container">\r\n    <hr/>\r\n    <footer class="footer">\r\n      ')
+        __M_writer(str( datetime.now().year))
+        __M_writer(' &copy - Jarrod Wilbur\r\n    </footer>\r\n  </div>\r\n\r\n    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>\r\n')
         __M_writer('    ')
         __M_writer(str( static_renderer.get_template_js(request, context)  ))
-        __M_writer('\r\n  \r\n  </body>\r\n</html>')
+        __M_writer('\r\n\r\n  </body>\r\n</html>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
 
 
-def render_content(context,**pageargs):
+def render_above_columns(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        def content():
-            return render_content(context)
+        def above_columns():
+            return render_above_columns(context)
         __M_writer = context.writer()
-        __M_writer('\r\n      Site content goes here in sub-templates.\r\n    ')
+        __M_writer('\r\n            this is area above the columns\r\n          ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_center_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def center_content():
+            return render_center_content(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n          <h1>center content</h1>\r\n        ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_right_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def right_content():
+            return render_right_content(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n          right content\r\n        ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_title(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def title():
+            return render_title(context)
+        __M_writer = context.writer()
+        __M_writer('Colonial Heritage Foundation')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_left_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def left_content():
+            return render_left_content(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n          left content\r\n        ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_maintenance_message(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def maintenance_message():
+            return render_maintenance_message(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n          <strong>Ye Be warned!</strong> This site is currently under construction.\r\n      ')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -63,6 +171,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"uri": "base.htm", "source_encoding": "utf-8", "line_map": {"64": 58, "34": 5, "35": 15, "36": 18, "37": 18, "38": 18, "43": 29, "44": 32, "45": 32, "46": 32, "17": 4, "19": 0, "52": 27, "58": 27, "28": 2, "29": 4, "30": 5}, "filename": "C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/base.htm"}
+{"source_encoding": "utf-8", "uri": "base.htm", "filename": "C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/base.htm", "line_map": {"64": 25, "130": 102, "69": 35, "136": 15, "74": 84, "142": 15, "79": 92, "17": 4, "19": 0, "84": 98, "124": 102, "89": 104, "90": 111, "91": 111, "92": 117, "93": 117, "94": 117, "160": 33, "148": 90, "100": 82, "166": 33, "39": 2, "40": 4, "41": 5, "106": 82, "172": 166, "45": 5, "46": 6, "154": 90, "112": 96, "50": 6, "118": 96, "55": 15, "56": 18, "57": 18, "58": 19, "59": 19, "60": 22, "61": 22, "62": 25, "63": 25}}
 __M_END_METADATA
 """
--- homepage/cached_templates/templates/right_content.html.py
+++ homepage/cached_templates/templates/right_content.html.py
@@ -5,13 +5,13 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1453239755.8526013
+_modified_time = 1453218279.2373192
 _enable_loop = True
-_template_filename = 'C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/index.html'
-_template_uri = 'index.html'
+_template_filename = 'C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/right_content.html'
+_template_uri = 'right_content.html'
 _source_encoding = 'utf-8'
 import os, os.path, re, json
-_exports = ['above_columns']
+_exports = ['right_content']
 
 
 def _mako_get_namespace(context, name):
@@ -29,28 +29,27 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def above_columns():
-            return render_above_columns(context._locals(__M_locals))
+        def right_content():
+            return render_right_content(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('\r\n\r\n')
-        __M_writer('\r\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'above_columns'):
-            context['self'].above_columns(**pageargs)
+        __M_writer('\r\n\r\n<div style="background-color: red">\r\n  ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'right_content'):
+            context['self'].right_content(**pageargs)
         
 
-        __M_writer('\r\n')
+        __M_writer('\r\n</div>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
 
 
-def render_above_columns(context,**pageargs):
+def render_right_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        def above_columns():
-            return render_above_columns(context)
+        def right_content():
+            return render_right_content(context)
         __M_writer = context.writer()
-        __M_writer('\r\n  this is more info\r\n')
+        __M_writer('\r\n    this is the right content\r\n  ')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -58,6 +57,6 @@ def render_above_columns(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"source_encoding": "utf-8", "uri": "index.html", "filename": "C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/index.html", "line_map": {"35": 1, "36": 9, "53": 10, "41": 12, "59": 53, "28": 0, "47": 10}}
+{"line_map": {"35": 1, "52": 4, "40": 6, "58": 52, "28": 0, "46": 4}, "filename": "C:/Users/jpwil_000/Documents/School/2016 Winter Classes/IS 413/Colonial_Heritage_Foundation/homepage/templates/right_content.html", "source_encoding": "utf-8", "uri": "right_content.html"}
 __M_END_METADATA
 """
